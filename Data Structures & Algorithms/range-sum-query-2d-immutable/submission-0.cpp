d9931fd487e5b7fe75a634b2806ab890eed548da
Uddesh Katyayan <uddeshkatyayan@gmail.com>
2026-06-13 01:47:08 +0530

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.cpp
@@ -0,0 +1,56 @@
+class NumMatrix {
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        prefix_sum.resize(n);
+        for(int i=0;i<n;i++){
+            prefix_sum[i].resize(m);
+        }
+        int sum = 0;
+        prefix_sum[0][0] = matrix[0][0];
+        for(int i=1;i<m;i++){
+            prefix_sum[0][i] = prefix_sum[0][i-1] + matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            prefix_sum[i][0] = prefix_sum[i-1][0] + matrix[i][0];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                prefix_sum[i][j] = matrix[i][j] + prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1];
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         cout << prefix_sum[i][j] << "\t";
+        //     }
+        //     cout << endl;
+        // }
+    }
+    
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        int total = prefix_sum[r2][c2];
+        cout << total << endl;
+        if(r1-1>=0){
+            // cout << " minus : " << prefix_sum[r1-1][c2] << endl;
+            total -= prefix_sum[r1-1][c2];
+        }
+        if(c1-1>=0){
+            // cout << " minus : " << prefix_sum[r2][c1-1] << endl;
+            total -= prefix_sum[r2][c1-1];
+        }
+        if(r1-1 >=0 && c1-1>=0){
+            // cout << " plus : " << prefix_sum[r1-1][c1-1] << endl;
+            total += prefix_sum[r1-1][c1-1];
+        }
+        return total;
+    }
+private:
+    vector<vector<int>> prefix_sum;
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
