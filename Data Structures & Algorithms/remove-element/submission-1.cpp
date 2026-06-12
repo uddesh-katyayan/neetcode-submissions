c73d1927494daa7e7db5bfe25660f21aa6537e4f
Uddesh Katyayan <uddeshkatyayan@gmail.com>
2026-06-12 10:12:50 +0530

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        while(right > 0 && nums[right]==val){
+            right--;
+        }
+        while(left<=right){
+            if(nums[left] == val){
+                swap(nums[left], nums[right]);
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        cout << left<< " " << right << endl;
+        return left;
+    }
+};
