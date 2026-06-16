592d129b972b3cb3516bfb2450f0ea9a03896078
Uddesh Katyayan <uddeshkatyayan@gmail.com>
2026-06-16 06:40:42 +0530

    Add: partition-labels - submission-4

---
+++ Data Structures & Algorithms/partition-labels/submission-4.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        int n = s.size();
+        vector<int> first(26, -1);
+        vector<int> last(26, -1);
+        for(int i=0;i<n;i++){
+            last[s[i]-'a'] = i;
+        }
+        int start = 0;
+        int end = 0;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            start = i;
+            end = last[s[i]-'a'];
+            int k = i;
+            while(k<=end && k<n){
+                int cur_end = last[s[k]-'a'];
+                end = max(end, cur_end);
+                if(k == end){
+                    ans.push_back(end-start+1);
+                    break;
+                } 
+                k++;
+            }
+            i = end;
+            // if(end==start){
+            //     ans.push_back(end - start + 1);
+            // } else{
+            //     int k = i + 1;
+            //     while(k<=end && k<n){
+            //         int cur_end = last[s[k]-'a'];
+            //         end = max(end, cur_end);
+            //         if(k == end){
+            //             ans.push_back(end-start+1);
+            //             break;
+            //         } 
+            //         k++;
+            //     }
+            //     i = end;
+            // }
+        }
+        return ans;
+    }
+};
